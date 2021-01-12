7d5e3cf4655f4e3608be60a4ef03d0d87aa6f137
Jakub Zochowski <jakub.zochowski997@gmail.com>
2021-01-12 22:59:26 +0000

    Add templating using pug

--- routes/shop.js
+++ routes/shop.js
@@ -3,11 +3,13 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    res.render('shop', { prods: adminData.products, pageTitle: 'Shop', path: '/'});
+    // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
